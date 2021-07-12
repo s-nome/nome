3440f72cebf60fc31658457433c89c59f2139743
kazuhe <omc.ohara@gmail.com>
2021-07-13 00:04:31 +0900

    feat: 本番環境動作確認

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 app.use(express.json())
+
+// todo: プロダクション環境のみ静的ファイルを提供
+app.use(express.static(path.join(__dirname, "../../client/dist")))
 
 app.get("/api", (req, res) => {
   const response = [
