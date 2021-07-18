f70b61ed7b85c23336ee5ac125f1a1300a2fbd64
Yutaka Kaneko <mail@ytk6565.net>
2021-07-18 22:53:39 +0900

    Feature: clientにTypeScriptの導入 (#20)
    * feat: client typescript

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -5,15 +5,16 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
+export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
   },
-};
+});
 </script>
 
 <style>
