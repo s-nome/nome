130304ba20a1b76b17dd895107340aec0ac5595a
Kazuhiro Ohara <57878514+kazuhe@users.noreply.github.com>
2021-07-27 09:18:51 +0900

    Feature: Textコンポーネント (#27)
    * feat: textコンポーネントのベース
    
    * feat: propsの設定とmdxの記述
    
    * feat: vueコンポーネントから呼び出し
    
    * fix: @でのimportエラーを解消する為にtsconfig.jsonを一部共通化

--- packages/client/.storybook/main.js
+++ packages/client/.storybook/main.js
@@ -1,10 +1,20 @@
+const { resolve } = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: resolve(__dirname, '../'),
+    })
+    return config
+  }
 }
---
+++ packages/client/src/components/atoms/BaseText/types.ts
@@ -0,0 +1,3 @@
+export type Size = 'small' | 'medium' | 'large'
+
+export type Color = 'default' | 'light' | 'alert'
