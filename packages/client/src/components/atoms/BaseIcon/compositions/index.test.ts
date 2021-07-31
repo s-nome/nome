da33d324c5deffd6e8c74242d3c195a64860862a
kazuhe <omc.ohara@gmail.com>
2021-08-01 01:47:43 +0900

    test: テストのtodoを記述

---
+++ packages/client/src/components/atoms/BaseIcon/compositions/index.test.ts
@@ -0,0 +1,17 @@
+import { toggleDisplayOfIconSelectionModal, changeIcon } from '.'
+
+describe('toggleDisplayOfIconSelectionModal', () => {
+  test.todo(
+    'アイコン選択画面が「表示されている」場合は関数の実行によって非表示になること'
+  )
+  test.todo(
+    'アイコン選択画面が「表示されていない」場合は関数の実行によって表示されること'
+  )
+})
+
+describe('changeIcon', () => {
+  test.todo('iconが既存のアイコンから新しいアイコンに変更されること')
+  test.todo(
+    'toggleDisplayOfIconSelectionModalがコールされて、アイコン選択画面が非表示になること'
+  )
+})
